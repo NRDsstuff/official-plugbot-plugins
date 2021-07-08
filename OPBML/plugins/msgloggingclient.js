3a983c274dcc0adf49cf85368c4ec93f14472952
NRD <85891552+NRDsstuff@users.noreply.github.com>
2021-07-08 16:58:00 +0200

    stuff

--- OPBML/plugins/msgloggingclient.js
+++ OPBML/plugins/msgloggingclient.js
@@ -8,19 +8,12 @@ module.exports = {
     func: function a(client){
         
         client.on('message', msg =>{
-
-            if(msg.channel.type === `dm`){
-                const logged = `
-${msg.author.username} with an id of ${msg.author.id}
-said ${msg.content} in dms.`
-                log.save(logged, './logs/OPBML/messages.txt')
-            }else{
-                const logged = `
+            const logged = `
 ${msg.author.username} with an id of ${msg.author.id}
 said ${msg.content} in channel ${msg.channel.name} with an id of ${msg.channel.id}
 (in server ${msg.guild.name})`
-                log.save(logged, './logs/OPBML/messages.txt')
-            }            
+
+            log.save(logged, './logs/OPBML/messages.txt')
         })    
     }
 }
